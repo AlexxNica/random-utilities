775a386bb8eabe7ea9fcec892248471b84945fe5
Alexandre Nicastro <kryndex@gmail.com>
2017-06-03 11:49:02 -0300

    adds jest, libdefs & fix .babelrc.js code consistency

--- .babelrc.js
+++ .babelrc.js
@@ -1,23 +1,23 @@
 // @flow
-"use strict";
+'use strict';
 
 const configPresets = [
-  ["env", {
-    "targets": {
-      "node": "current",
-      "browsers": ["last 2 versions"]
+  ['env', {
+    'targets': {
+      'node': 'current',
+      'browsers': ['last 2 versions']
     },
-    "useBuiltIns": "usage",
-    "debug": true
+    'useBuiltIns': 'usage',
+    'debug': true
   }],
-  "stage-0",
-  "flow",
-  "react"
+  'stage-0',
+  'flow',
+  'react'
 ];
 
 const configPlugins = [
-  "lodash",
-  "transform-flow-strip-types"
+  'lodash',
+  'transform-flow-strip-types'
 ];
 
 module.exports = {
